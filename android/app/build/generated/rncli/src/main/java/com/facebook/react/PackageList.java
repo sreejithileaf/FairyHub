2adac819f73fcde72eb128a599470cde25b1c8e2
sreejith-ileaf <sreejith@ileafsolutions.net>
2022-05-31 18:07:59 +0530

    Cart UI changes partially done

---
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -0,0 +1,172 @@
+
+package com.facebook.react;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainPackageConfig;
+import com.facebook.react.shell.MainReactPackage;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+// @react-native-async-storage/async-storage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+// @react-native-community/blur
+import com.cmcewen.blurview.BlurViewPackage;
+// @react-native-community/geolocation
+import com.reactnativecommunity.geolocation.GeolocationPackage;
+// @react-native-community/masked-view
+import org.reactnative.maskedview.RNCMaskedViewPackage;
+// @react-native-community/netinfo
+import com.reactnativecommunity.netinfo.NetInfoPackage;
+// @react-native-community/toolbar-android
+import com.reactnativecommunity.toolbarandroid.ReactToolbarPackage;
+// @react-native-firebase/analytics
+import io.invertase.firebase.analytics.ReactNativeFirebaseAnalyticsPackage;
+// @react-native-firebase/app
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
+// @react-native-firebase/dynamic-links
+import io.invertase.firebase.dynamiclinks.ReactNativeFirebaseDynamicLinksPackage;
+// @react-native-google-signin/google-signin
+import com.reactnativegooglesignin.RNGoogleSigninPackage;
+// @react-native-picker/picker
+import com.reactnativecommunity.picker.RNCPickerPackage;
+// @sentry/react-native
+import io.sentry.react.RNSentryPackage;
+// react-native-adjust
+import com.adjust.nativemodule.AdjustPackage;
+// react-native-date-picker
+import com.henninghall.date_picker.DatePickerPackage;
+// react-native-device-info
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+// react-native-fast-image
+import com.dylanvann.fastimage.FastImageViewPackage;
+// react-native-fbsdk
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+// react-native-fingerprint-scanner
+import com.hieuvp.fingerprint.ReactNativeFingerprintScannerPackage;
+// react-native-gesture-handler
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+// react-native-linear-gradient
+import com.BV.LinearGradient.LinearGradientPackage;
+// react-native-localize
+import com.reactcommunity.rnlocalize.RNLocalizePackage;
+// react-native-onesignal
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
+// react-native-orientation
+import com.github.yamill.orientation.OrientationPackage;
+// react-native-pager-view
+import com.reactnativepagerview.PagerViewPackage;
+// react-native-reanimated
+import com.swmansion.reanimated.ReanimatedPackage;
+// react-native-restart
+import com.avishayil.rnrestart.ReactNativeRestartPackage;
+// react-native-safe-area-context
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+// react-native-screens
+import com.swmansion.rnscreens.RNScreensPackage;
+// react-native-share
+import cl.json.RNSharePackage;
+// react-native-snackbar
+import com.azendoo.reactnativesnackbar.SnackbarPackage;
+// react-native-splash-screen
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+// react-native-touch-id
+import com.rnfingerprint.FingerprintAuthPackage;
+// react-native-vector-icons
+import com.oblador.vectoricons.VectorIconsPackage;
+// react-native-video
+import com.brentvatne.react.ReactVideoPackage;
+// react-native-webview
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+// rn-fetch-blob
+import com.RNFetchBlob.RNFetchBlobPackage;
+// rn-range-slider
+import com.ashideas.rnrangeslider.RangeSliderPackage;
+
+public class PackageList {
+  private Application application;
+  private ReactNativeHost reactNativeHost;
+  private MainPackageConfig mConfig;
+
+  public PackageList(ReactNativeHost reactNativeHost) {
+    this(reactNativeHost, null);
+  }
+
+  public PackageList(Application application) {
+    this(application, null);
+  }
+
+  public PackageList(ReactNativeHost reactNativeHost, MainPackageConfig config) {
+    this.reactNativeHost = reactNativeHost;
+    mConfig = config;
+  }
+
+  public PackageList(Application application, MainPackageConfig config) {
+    this.reactNativeHost = null;
+    this.application = application;
+    mConfig = config;
+  }
+
+  private ReactNativeHost getReactNativeHost() {
+    return this.reactNativeHost;
+  }
+
+  private Resources getResources() {
+    return this.getApplication().getResources();
+  }
+
+  private Application getApplication() {
+    if (this.reactNativeHost == null) return this.application;
+    return this.reactNativeHost.getApplication();
+  }
+
+  private Context getApplicationContext() {
+    return this.getApplication().getApplicationContext();
+  }
+
+  public ArrayList<ReactPackage> getPackages() {
+    return new ArrayList<>(Arrays.<ReactPackage>asList(
+      new MainReactPackage(mConfig),
+      new AsyncStoragePackage(),
+      new BlurViewPackage(),
+      new GeolocationPackage(),
+      new RNCMaskedViewPackage(),
+      new NetInfoPackage(),
+      new ReactToolbarPackage(),
+      new ReactNativeFirebaseAnalyticsPackage(),
+      new ReactNativeFirebaseAppPackage(),
+      new ReactNativeFirebaseDynamicLinksPackage(),
+      new RNGoogleSigninPackage(),
+      new RNCPickerPackage(),
+      new RNSentryPackage(),
+      new AdjustPackage(),
+      new DatePickerPackage(),
+      new RNDeviceInfo(),
+      new FastImageViewPackage(),
+      new FBSDKPackage(),
+      new ReactNativeFingerprintScannerPackage(),
+      new RNGestureHandlerPackage(),
+      new LinearGradientPackage(),
+      new RNLocalizePackage(),
+      new ReactNativeOneSignalPackage(),
+      new OrientationPackage(),
+      new PagerViewPackage(),
+      new ReanimatedPackage(),
+      new ReactNativeRestartPackage(),
+      new SafeAreaContextPackage(),
+      new RNScreensPackage(),
+      new RNSharePackage(),
+      new SnackbarPackage(),
+      new SplashScreenReactPackage(),
+      new FingerprintAuthPackage(),
+      new VectorIconsPackage(),
+      new ReactVideoPackage(),
+      new RNCWebViewPackage(),
+      new RNFetchBlobPackage(),
+      new RangeSliderPackage()
+    ));
+  }
+}
